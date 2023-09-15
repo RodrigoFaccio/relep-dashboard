8eacdc0d059da2993f07702a625c73e6431b0fd3
RodrigoFaccio <rodrigo.facio@yogha.com>
2023-09-15 10:17:04 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import DashboardComun from "./pages/DashboardComun";
-
+//@ts-ignore
 function App() {
   return (
-  <DashboardComun/>
+    <div>
+    <h2>Player de Vídeo Simples em React</h2>
+    <video controls width="600">
+      <source src="file:///Users/rodrigofacio/Downloads/uber/uber2.mp4" type="video/mp4" />
+      Seu navegador não suporta o elemento de vídeo.
+    </video>
+  </div>
   );
 }
 
